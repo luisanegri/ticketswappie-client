bac737a8ac191c36c3da264d3f5d2d4bd7220777
luisanegri <luisaanegri@gmail.com>
2019-11-26 13:41:28 +0100

    change return read_event

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,7 +1,7 @@
 export default function(state = [], action = {}) {
   switch (action.type) {
     case 'CREATE_EVENT':
-      return [...state, action.payload];
+      return action.payload;
     case 'READ_EVENTS':
       return action.payload;
     case 'READ_EVENT':
