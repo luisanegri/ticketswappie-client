1bf0a0d5523c7d91bb09a90e43d6bb3c4bd830f5
luisanegri <luisaanegri@gmail.com>
2020-08-05 14:53:28 +0200

    comment out no events conditional

--- src/components/Events/EventList.js
+++ src/components/Events/EventList.js
@@ -3,11 +3,11 @@ import { Row, Col, Card, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function EventList({ events, loading, error }) {
-  const empty = !events.length;
+  // const empty = !events.length;
 
-  if (empty) {
-    return <p className="before">No events</p>;
-  }
+  // if (empty) {
+  //   return <p className="before">No events</p>;
+  // }
 
   if (loading) {
     return <div className="before">Loading...</div>;
